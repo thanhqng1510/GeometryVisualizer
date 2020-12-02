366b812d33465dc9dda46d45a7b14a913fa0013e
hoangnam <namnguyen.fdev@gmail.com>
2020-12-02 16:49:17 +0700

    Login

---
+++ src/main/java/MainLogin.java
@@ -0,0 +1,7 @@
+public class MainLogin {
+    public static void main(String[] args)   {
+        Login login= new Login();
+        //SignUp su= new SignUp();
+//        ResetPassword rp= new ResetPassword();
+    }
+}
--- src/main/java/MongoDb.java
+++ src/main/java/MongoDb.java
@@ -1,6 +1,8 @@
 import com.mongodb.*;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.util.JSON;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
@@ -13,12 +15,19 @@ public class MongoDb {
     private static MongoDatabase mongoDb = mongoClient.getDatabase("Java");
     private static MongoCollection collection = (MongoCollection) mongoDb.getCollection("user");
 
-    public static void addNewUser(String username,String password) {
-        Document document = new Document("username",username);
-        document.append("password",password);
-        collection.insertOne(document);
-        System.out.println("User has been added");
+    public static boolean addNewUser(String email,String username,String password) {
+        Document check = (Document) collection.find(Filters.eq("email", email)).first();
+        if (check != null){
+            return false;
+        }else{
+            Document document = new Document("email",email);
+            document.append("username",username);
+            document.append("password",password);
+            collection.insertOne(document);
+            return true;
+        }
     }
+
 
     public static boolean authenticatedUser(String username,String password){
         Document document = new Document("username",username);
@@ -42,10 +51,24 @@ public class MongoDb {
         }
     }
 
+    public static int login(String email,String password){
+        Document check = (Document) collection.find(Filters.eq("email", email)).first();
+        if (check == null){
+            return 1;
+        }else{
+            String docPass= (String) check.get("password");
+            if (docPass.trim().equals(password.trim())){
+                return 0;
+            }else{
+                return 2;
+            }
+        }
+    }
+
     public static void main(String[] args) throws UnknownHostException {
         System.out.println("Database Connected");
 
-        addNewUser("namnguyen","namnguyen21");
+        //addNewUser("namnguyen","namnguyen21");
         //if (authenticatedUser("namnguyen","namnguyen21") == true){
         //    updateUserAccount("namle","namle","namnguyen","namnguyen21");
         //}
